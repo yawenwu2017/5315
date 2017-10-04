6ba9d8e1ddb87336fb5426dd276270f75c261886
yawenwu2017 <wuy8@seattleu.edu>
2017-10-04 16:43:18 -0700

    hw1 commit

--- web/5315/empdata.js
+++ web/5315/empdata.js
@@ -5,8 +5,88 @@
       "designation": "Assurance Staff",
       "salary": "$170,750",
       "joining_date": "2011/07/25",
-      "office": "Tokyo",
-      "extension": "8422"
-    }
+      "city": "Tokyo",
+      "staff id": "8420"
+    },
+    {
+      "name": "Mary Jane",
+      "designation": "Assurance Staff",
+      "salary": "$180,750",
+      "joining_date": "2012/07/25",
+      "city": "Kyoko",
+      "staff id": "8421"
+    },
+    {
+      "name": "Jane Williams",
+      "designation": "Assurance Staff",
+      "salary": "$180,750",
+      "joining_date": "2012/09/25",
+      "city": "Kyoko",
+      "staff id": "8422"
+    },
+    {
+      "name": "Jill Wayeman",
+      "designation": "Assurance Staff",
+      "salary": "$180,750",
+      "joining_date": "2012/09/25",
+      "city": "Seattle",
+      "staff id": "8423"
+    },
+    {
+      "name": "Jacky Chen",
+      "designation": "Assurance Staff",
+      "salary": "$180,750",
+      "joining_date": "2012/09/25",
+      "city": "Hongkong",
+      "staff id": "8424"
+    },
+    {
+      "name": "Kate York",
+      "designation": "Assurance Staff",
+      "salary": "$180,750",
+      "joining_date": "2012/09/25",
+      "city": "Singapore",
+      "staff id": "8425"
+    },
+    {
+      "name": "Linda Chen",
+      "designation": "Assurance Staff",
+      "salary": "$180,750",
+      "joining_date": "2013/09/25",
+      "city": "Hongkong",
+      "staff id": "8426"
+    },
+    {
+      "name": "Terry Xu",
+      "designation": "Assurance Staff",
+      "salary": "$180,750",
+      "joining_date": "2012/09/25",
+      "city": "Shanghai",
+      "staff id": "8427"
+    },
+    {
+      "name": "Ming Jin",
+      "designation": "Assurance Staff",
+      "salary": "$180,750",
+      "joining_date": "2012/09/25",
+      "city": "Beijing",
+      "staff id": "8428"
+    },
+    {
+      "name": "Tim Cook",
+      "designation": "Assurance Staff",
+      "salary": "$180,750",
+      "joining_date": "2013/09/25",
+      "city": "Orange County",
+      "staff id": "8429"
+    },
+    {
+      "name": "Steve Jobs",
+      "designation": "Assurance Staff",
+      "salary": "$180,750",
+      "joining_date": "2013/09/25",
+      "city": "Taipei",
+      "staff id": "8430"
+    },
   ]
 }
